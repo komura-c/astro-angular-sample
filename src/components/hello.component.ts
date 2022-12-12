eb4403eeb3ec064b7b8f831228dc3b20e807e772
komura-c <affirman03139@outlook.jp>
2022-12-12 11:51:14 +0900

    fix

---
+++ src/components/hello.component.ts
@@ -0,0 +1,22 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  imports: [NgIf],
+  template: `
+    <p>Hello from Angular!!</p>
+
+    <button (click)="toggle()">Toggle</button>
+
+    <p *ngIf="show">Toggled</p>
+  `,
+})
+export class HelloComponent {
+  show = true;
+
+  toggle() {
+    this.show = !this.show;
+  }
+}
